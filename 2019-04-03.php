dc3d183929c179add5bb1741173ea803e8f4f93c
renxq <rxqphper@163.com>
2019-04-03 22:04:42 +0800

    2019-04-03

---
+++ 2019-04-03.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Desc: 2元买1瓶酒，2个空瓶换1瓶酒，4个瓶盖换1瓶酒，10元能喝多少瓶酒？
+ * User: renxinqiang
+ * Date: 2019/4/3
+ * Time: 下午9:15
+ */
+
+/**
+ * 不考虑借老板一瓶的情况
+ */
+$num = 0;
+$qian = 10;
+$pinggai = 0;
+$jiuping = 0;
+$mai = 2;
+
+while(true){
+    if ($qian<=0 && $jiuping<2 && $pinggai<4) {
+        break;
+    }
+    if ($qian>0) {
+        $qian -= $mai;
+        $jiuping++;
+        $pinggai++;
+        $num++;
+        continue;
+    }
+    if ($jiuping>=2) {
+        $num+=floor($jiuping/2);
+        $pinggai += floor($jiuping/2);
+        $jiuping = floor($jiuping/2) + $jiuping%2;
+    }
+
+    if ($pinggai>=4) {
+        $num+=floor($pinggai/4);
+        $jiuping += floor($pinggai/4);
+        $pinggai = floor($pinggai/4) + $pinggai%4;
+    }
+
+
+}
+echo $num;
--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 * [有 n 个硬币，其中 1 个为假币，假币重量较轻，你有一把天平，请问，至少需要称多少次能保证一定找到假币?](https://github.com/renxinqiang/Algorithm/blob/master/2019-03-31.php)
 * [二分查找](https://github.com/renxinqiang/Algorithm/blob/master/2019-04-01.php)
 * [最大数: 给定一个非负整形列表,重新排列顺序组成一个最大的整数 注:结果会很大,返回字符串即可 例如: \[2,34,5,6,91\]=> 9165342](https://github.com/renxinqiang/Algorithm/blob/master/2019-04-02.php)
+* [2元买1瓶酒，2个空瓶换1瓶酒，4个瓶盖换1瓶酒，10元能喝多少瓶酒？](https://github.com/renxinqiang/Algorithm/blob/master/2019-04-03.php)
