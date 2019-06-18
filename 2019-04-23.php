b9638ae886aa6a8fa41cf25956dc9b394df03977
renxq <rxqphper@163.com>
2019-06-18 14:51:47 +0800

    fix

--- 2019-04-23.php
+++ 2019-04-23.php
@@ -13,44 +13,22 @@
  */
 
 
-$a = [1,3];
-$b = [2];
+$a = [3];
+$b = [-2,-1];
 
-function sum($a, $b){
-    $a_count = count($a);
-    $b_count = count($b);
-    if ($a_count == 0) {
-        $a_mid = array_sum($a);
-    } elseif ($a_count<2) {
-        $a_mid = array_sum($a) / $a_count;
+function sum($nums1, $nums2){
+    $array = array_merge($nums1, $nums2);
+    sort($array);
+    $count = count($array);
+    $ou = $ji = false;
+    if ($count % 2 == 0) {
+        $mid = $count / 2;
+        $ou = true;
     } else {
-        if ($a_count%2==0) {
-            $a_mid = ($a[$a_count/2] + $a[$a_count/2-1])/2;
-        } else {
-            $a_mid = $a[ceil($a_count/2) - 1];
-        }
+        $mid = floor($count / 2);
+        $ji = true;
     }
-
-    if ($b_count == 0) {
-        $b_mid = array_sum($b);
-    } elseif ($b_count<2) {
-        $b_mid = array_sum($b) / $b_count;
-    } else {
-        if ($b_count%2==0) {
-            $b_mid = ($b[$b_count/2] + $b[$b_count/2-1])/2;
-        } else {
-            $b_mid = $b[ceil($b_count/2) - 1];
-        }
-    }
-
-    if ($a_count && $b_count) {
-        return ($a_mid + $b_mid) / 2;
-    } elseif ($a_count == 0 && $b_count != 0) {
-        return $b_mid;
-    } else {
-        return $a_mid;
-    }
-
+    return $ou ? ($array[$mid] + $array[$mid+1]) / 2 : $array[$mid];
 }
 
 echo sum($a, $b);// 10.5
